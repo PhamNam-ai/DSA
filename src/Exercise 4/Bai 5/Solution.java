1ebaae1f25b00b09b266337321379be042fb24b1
phamnam-data-ai <24021579@vnu.edu.vn>
2025-11-05 09:28:25 +0700

    Initial commit

--- src/Exercise 4/Bai 5/Solution.java
+++ src/Exercise 4/Bai 5/Solution.java
@@ -1,3 +1,15 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
 class Result {
 
     /*
@@ -31,53 +43,32 @@ class Result {
 
         return result;
     }
-    private static void mergeSort(List<Integer> arr, int left, int right) {
-        if (left < right) {
-            int mid = (left + right) / 2;
 
-            mergeSort(arr, left, mid);
-            mergeSort(arr, mid + 1, right);
 
-            merge(arr, left, mid, right);
-        }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        List<Integer> result = Result.closestNumbers(arr);
+
+        bufferedWriter.write(
+                result.stream()
+                        .map(Object::toString)
+                        .collect(joining(" "))
+                        + "\n"
+        );
+
+        bufferedReader.close();
+        bufferedWriter.close();
     }
-
-    private static void merge(List<Integer> arr, int left, int mid, int right) {
-        int n1 = mid - left + 1;
-        int n2 = right - mid;
-
-        int[] L = new int[n1];
-        int[] R = new int[n2];
-
-        for (int i = 0; i < n1; i++)
-            L[i] = arr.get(left + i);
-        for (int j = 0; j < n2; j++)
-            R[j] = arr.get(mid + 1 + j);
-
-        int i = 0, j = 0, k = left;
-
-        while (i < n1 && j < n2) {
-            if (L[i] <= R[j]) {
-                arr.set(k, L[i]);
-                i++;
-            } else {
-                arr.set(k, R[j]);
-                j++;
-            }
-            k++;
-        }
-        while (i < n1) {
-            arr.set(k, L[i]);
-            i++;
-            k++;
-        }
-        while (j < n2) {
-            arr.set(k, R[j]);
-            j++;
-            k++;
-        }
-    }
-
-
-
 }

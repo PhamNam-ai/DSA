8ea7ee6696842a65465dda8ccb14ca46c2f705fd
phamnam-data-ai <24021579@vnu.edu.vn>
2025-11-10 21:20:02 +0700

    Initial commit

--- src/Exercise 11/Bai 6/Solution.java
+++ src/Exercise 11/Bai 6/Solution.java
@@ -1,80 +1,26 @@
-import java.io.*;
-import java.util.*;
+public static List<Double> runningMedian(List<Integer> a) {
+    // Write your code here
+    PriorityQueue<Integer> minPQ = new PriorityQueue<>();
+    PriorityQueue<Integer> maxPQ = new PriorityQueue<>(Collections.reverseOrder());
+    List<Double> result = new ArrayList<>();
 
-class Result {
+    for (int num : a) {
+        if (maxPQ.isEmpty() || num <= maxPQ.peek()) maxPQ.add(num);
+        else minPQ.add(num);
 
-    static class Node implements Comparable<Node> {
-        int value;
-        int index;
+        if (maxPQ.size() > minPQ.size() + 1) minPQ.add(maxPQ.poll());
+        else if (minPQ.size() > maxPQ.size() + 1) maxPQ.add(minPQ.poll());
 
-        Node(int value, int index) {
-            this.value = value;
-            this.index = index;
-        }
+        double median;
+        if (maxPQ.size() == minPQ.size())
+            median = (maxPQ.peek() + minPQ.peek()) / 2.0;
+        else if (maxPQ.size() > minPQ.size())
+            median = maxPQ.peek();
+        else
+            median = minPQ.peek();
 
-        public int compareTo(Node other) {
-            if (this.value != other.value) return Integer.compare(this.value, other.value);
-            return Integer.compare(this.index, other.index);
-        }
+        result.add(median);
     }
 
-    public static List<Double> runningMedian(List<Integer> a) {
-        List<Double> res = new ArrayList<>();
-        TreeSet<Node> tree = new TreeSet<>();
-        Node mid = null;
-
-        for (int i = 0; i < a.size(); i++) {
-            Node node = new Node(a.get(i), i);
-            tree.add(node);
-
-            if (mid == null) {
-                mid = node;
-            } else if (a.get(i) < mid.value || (a.get(i) == mid.value && i < mid.index)) {
-                if (tree.headSet(mid).size() > tree.tailSet(mid, false).size())
-                    mid = tree.lower(mid);
-            } else {
-                if (tree.tailSet(mid, false).size() > tree.headSet(mid).size())
-                    mid = tree.higher(mid);
-            }
-
-            int size = tree.size();
-            if (size % 2 == 1) {
-                res.add((double) mid.value);
-            } else {
-                Node left = mid;
-                Node right = tree.higher(mid);
-                res.add((left.value + right.value) / 2.0);
-            }
-        }
-
-        return res;
-    }
+    return result;
 }
-
-public class Solution {
-    public static void main(String[] args) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
-
-        int aCount = Integer.parseInt(bufferedReader.readLine().trim());
-        List<Integer> a = new ArrayList<>();
-
-        for (int i = 0; i < aCount; i++) {
-            int aItem = Integer.parseInt(bufferedReader.readLine().trim());
-            a.add(aItem);
-        }
-
-        List<Double> result = Result.runningMedian(a);
-        for (int i = 0; i < result.size(); i++) {
-            bufferedWriter.write(String.valueOf(result.get(i)));
-            if (i != result.size() - 1) bufferedWriter.write("\n");
-        }
-        bufferedWriter.newLine();
-
-        bufferedReader.close();
-        bufferedWriter.close();
-    }
-}
-
-
-Ex : a = [5, 15, 1, 3]
